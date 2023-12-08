6f4a6f8fe1baae32b8796505b36bf39d262d22a1
Densex4545 <147789317+Densetsu4545@users.noreply.github.com>
2023-12-08 15:43:04 +0900

    ftyfytflag

--- _flag.py
+++ _flag.py
@@ -1,21 +1,18 @@
 from PIL import Image
 import os
 
-def main():
-    if not os.path.exists("small"):
-        os.makedirs("small")
-    if not os.path.exists("medium"):
-        os.makedirs("medium")
-    print("country name")
-    country_name = str(input())
-    flag = Image.open("_flag.py") 
-    medium_flag = flag.resize(41,26)
-    small_flag = flag.resize(10,7)
+if not os.path.exists("small"):
+    os.makedirs("small")
+if not os.path.exists("medium"):
+    os.makedirs("medium")
+print("country name")
+country_name = str(input())
+flag = Image.open("_flag.tga") 
 
-    flag.save("{}.tga")
-    medium_flag.save("medium/{}.tga")
-    small_flag.save("small/{}.tga")
+medium_flag = flag.resize(41,26)
 
+small_flag = flag.resize(10,7)
 
-
-    
+flag.save("{}.tga".format(country_name))
+medium_flag.save("medium/{}.tga".format(country_name))
+small_flag.save("small/{}.tga".format(country_name))
